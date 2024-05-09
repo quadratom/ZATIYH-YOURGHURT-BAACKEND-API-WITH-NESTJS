a089c9171910ebe89175ba485c8bb3423be3c767
gabrielopeyemi <famosipe2010@gmail.com>
2024-05-09 22:52:00 +0100

    add  product service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { UsersModule } from './users/users.module';
 import { dbConfig } from './config';
 import { ZohoModule } from './zoho/zoho.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './product/product.module';
 
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot(dbConfig.db), ZohoModule, ConfigModule.forRoot()],
+  imports: [UsersModule, MongooseModule.forRoot(dbConfig.db), ZohoModule, ConfigModule.forRoot(), ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
